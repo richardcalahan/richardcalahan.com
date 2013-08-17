cf3b9dcc4cfe92e6ceeb2e7b9a9acd9d9ad51c28
richardcalahan <richard@calahan.me>
2013-08-17 15:32:24 -0400

    cool url idea

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Navigation highlighting
     if ( /^\/(\w*)\//.test(window.location.pathname) ) {
         var category = window.location.pathname.match(/^\/(\w*)\//)[1];
         $('a[href="/' + category + '/"]').addClass('current');
