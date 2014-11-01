3de69af6fa34ca8decb1fed5f32faf63a9481061
Richard Calahan <richard@calahan.me>
2014-11-01 14:26:29 -0400

    static site

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    // Navigation highlighting
-    if ( /^\/(\w*)\//.test(window.location.pathname) ) {
-        var category = window.location.pathname.match(/^\/(\w*)\//)[1];
-        $('a[href="/' + category + '/"]').addClass('current');
-    }
-
-});
